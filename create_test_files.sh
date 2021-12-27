6067f2212eacaabfed932cc779520674759660bc
SamoKopecky <sano.kopecky@gmail.com>
2021-12-27 16:41:42 +0100

    Improve script with creating templates instead of empty files

--- create_test_files.sh
+++ create_test_files.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
+BASE_DIR=$(pwd)
+
 create_file () {
 	TEMP=""
 	printf -v TEMP "%s%s" $1 $2
-	touch $TEMP
+	if [ ! -f "$TEMP" ]; then
+		cat $BASE_DIR/templates/$2 > $TEMP
+		echo "$TEMP does not already exist, creating"
+	fi
 }
 
-BASE_DIR=$(pwd)
 while read p; do
 	cd $BASE_DIR
 	NAME=$(echo $p | sed 's/ //g')
-	mkdir $NAME
+	if [ ! -d "$NAME" ]; then
+		mkdir $NAME
+		echo "$NAME does not already exist, creating"
+	fi
 	cd $NAME
 	create_file $NAME "Test.txt"	
 	create_file $NAME "Útok.txt"	
